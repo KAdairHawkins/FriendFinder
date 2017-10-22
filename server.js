f3295f41e0512ca28c9b963260114fc04c5a2b8b
Adair Hawkins <adairhawkins@gmail.com>
2017-10-22 09:35:04 -0500

    new stuff

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+require('./app/routing/htmlRoutes')(app);
+
+app.listen(5555, function(){
+  console.log("App listening on PORT" + 5555);
+});
